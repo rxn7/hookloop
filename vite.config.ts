7db2fa1d6079dc189a39322eeae852a5bb963c10
rxn7 <rotthin_dev@protonmail.com>
2025-11-30 14:53:38 +0100

    Fixed vite config to load assets properly on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	base: "/hookloop/",
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
